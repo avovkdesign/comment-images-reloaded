e07ed7e5facdb9c85b80bdf0197f38ffe16d8e94
Alexandra Vovk <avovkdesign@gmail.com>
2016-02-01 17:09:27 +0200

    версия 2.0.1

--- comment-image.php
+++ comment-image.php
@@ -3,7 +3,7 @@
  *	Plugin Name: Comment Image Reloaded 
  *	Description: It allows visitors to attach images to comments. This plugin is better version of <a href="https://wordpress.org/plugins/comment-images/">Comment Image by Tom McFarlin</a> that allow you control imase size. And it does <strong>only to sql query to get all comments</strong> (not one query for one comment).
  *	Plugin URI: http://wp-puzzle.com/comment-images-reloaded/
- *	Version: 2.0
+ *	Version: 2.0.1
  *  Author: WP Puzzle 
  *  Author URI: http://wp-puzzle.com/
  * ==================================================================================================== */
